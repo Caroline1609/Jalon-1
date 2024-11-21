4f18f2cd5c7520971e5eccab3486535402eea8cc
Caroline <70326049+Caroline1609@users.noreply.github.com>
2024-11-21 10:56:03 +0100

    UPLOAD

--- exo.java
+++ exo.java
@@ -1,3 +1,5 @@
+
+
 /* Programme à réaliser :
  
 [Réception d'un message électronique] 
@@ -15,39 +17,73 @@ Le destinataire a peut-être activé un message d’absence (réponse automatiqu
 public class exo {
     public static void main(String[] args) {
                 
-        String EmailDesti = "Caroline.XXXXXXX@wwwwwwww.ccc"; //entre le mail destinataire
-        String verifEmailDesti = "Caroline.XXXXXXX@wwwww.ccc"; // On vérifie si elle est valide.
-        int reponse;
+        String EmailDesti = "rrrrr.XXXXXXX@wwwwwwww.ccc"; // Le mail du destinataire.
+        String verifEmailDesti = "rrrrrr.XXXXXXX@wwwwwwww.ccc"; // On vérifie si elle est valide.
 
-        int DestiEspace = 35; // Espace disponible dans la boite mail
-        int Destiverifemail = 20; // verification du mail
+        int DestiEspace = 35; // Espace disponible dans la boite mail du destinataire.
+        int Destiverifemail = 85; // Vérification de la taille de l'email.
 
-        int analyseAV = 25; // L'analyse antivirus n'accepte pas les PJ inférieur à 25 Mo 
-        int Pj = 30; // PJ a 30 mo.
+        int analyseAV = 25; // L'analyse antivirus qui n'accepte pas les Pièces joints inférieur à 25 Mo.
+        int Pj = 0; // taille de la pièce jointe.
+
+        int MessageAuto = 0; // Message automatique
+
 
         
-        
+        if (EmailDesti.equals(verifEmailDesti) && Destiverifemail < DestiEspace ){ // Adresse mail ok et taille de l'email ok
 
-        if (Destiverifemail < DestiEspace && Pj >= analyseAV ){
+            System.out.println("Message envoyer"); //Afficher.
 
-            System.out.println(EmailDesti.equals(verifEmailDesti));
-            System.out.println("Envoi du mail");
+        } else if (EmailDesti.equals(verifEmailDesti) && Destiverifemail < DestiEspace && Pj >= analyseAV ) { //Adresse mail ok et taille de l'email ok et pièce jointe ok .
+            System.out.println("Message envoyer"); //Afficher.
+            
+        } else if (EmailDesti.equals(verifEmailDesti) && Destiverifemail < DestiEspace && Pj >= analyseAV && MessageAuto ==1 ){ // si le mail rempli toute les conditions. Et que le destinaire est mis un message automatique.
 
+            System.out.println("Message envoyer"); //Afficher.
+            System.out.println("Message automatique de votre destinaire : "); //Afficher.
+            System.out.println("Je suis en vacance, je vous réponderais utérieurement"); //Afficher.
 
-
-
-        } else {
-            System.out.println("Non envoi du mail");
+        } else { // sinon si une des conditions des fausses renvoyer.
+            System.out.println("envoi impossible, veuillez respecter les régles d'envoi de mail."); // afficher
         }
+
+
+        //Vérification.
+
+        System.out.println("Vérification");
+        System.out.println("Vérification du mail");
         
+        if (EmailDesti.equals(verifEmailDesti)){ // Vérification que le mail est correct
+            System.out.println("Email existe");
+        } else{
+            System.out.println("Email invalide");
+        }
 
+        System.out.println("Vérification taille du mail");
+        if (Destiverifemail < DestiEspace){ //Vérifie si le destinataire a assez espace.
+            System.out.println("Assez de place");
+        } else {
+            System.out.println("Pas assez de place");
+        }
+
+
+        System.out.println("Vérification de la pièce jointe (s'il y en n'a)");
+        if (Pj >= analyseAV){ //Analyse de la pièce jointe
+            System.out.println("Pièce jointe conforme");
+        } else if (Pj < 25){
+            System.out.println("pièce jointe inférieur");
+        } else if (Pj <= 0){
+            System.out.println("Pas de pièce jointes");
+        }
+            
     }
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
